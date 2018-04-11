65667fd2c17a50a7f12516ecbc83838e95276a3b
Pablo Bello <thefactus@gmail.com>
2018-04-11 18:47:14 -0300

    add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
   namespace :admin do
     get '', to: 'dashboard#index', as: '/'

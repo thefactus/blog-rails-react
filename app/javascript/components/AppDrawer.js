e0d1b7bdcdd393de9279c7591af1f91804fc9059
Pablo Bello <thefactus@gmail.com>
2018-04-11 18:46:37 -0300

    update app drawer

--- app/javascript/components/AppDrawer.js
+++ app/javascript/components/AppDrawer.js
@@ -8,6 +8,7 @@ import List from "material-ui/List";
 import Typography from "material-ui/Typography";
 import Divider from "material-ui/Divider";
 import { mailFolderListItems, otherMailFolderListItems } from "./tileData";
+import Button from "material-ui/Button";
 
 const drawerWidth = 240;
 
@@ -20,6 +21,11 @@ const styles = theme => ({
     position: "relative",
     display: "flex",
     height: "100vh"
+  },
+  button: {
+    // margin: theme.spacing.unit,
+    float: "right",
+    textAlign: "right"
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1
@@ -37,7 +43,17 @@ const styles = theme => ({
   mainContent: {
     marginTop: theme.spacing.unit * 3
   },
-  toolbar: theme.mixins.toolbar
+  toolbar: theme.mixins.toolbar,
+  toolbarDisplay: {
+    display: "flex"
+  },
+  tools: {
+    flexGrow: 1,
+    margin: 0
+  },
+  title: {
+    marginTop: 10
+  }
 });
 
 class AppDrawer extends React.Component {
@@ -66,7 +82,21 @@ class AppDrawer extends React.Component {
         </Drawer>
         <main className={classes.content}>
           <div className={classes.toolbar} />
-          <Typography variant="title">{this.props.title}</Typography>
+          <div className={classes.toolbarDisplay}>
+            <Typography variant="title" className={classes.title}>
+              {this.props.title}
+            </Typography>
+            <ul className={classes.tools}>
+              <Button
+                variant="raised"
+                color="primary"
+                className={classes.button}
+                href="/admin/posts/new"
+              >
+                New Post
+              </Button>
+            </ul>
+          </div>
           <div className={classes.mainContent}>{this.props.content}</div>
         </main>
       </div>

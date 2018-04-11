02b8cb52e162204e69ed7c962e73d375bf87ef38
Pablo Bello <thefactus@gmail.com>
2018-04-11 18:41:26 -0300

    update admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,8 @@
 class AdminController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin!
+
+  def authenticate_admin!
+    render_404 unless current_user.admin?
+  end
 end

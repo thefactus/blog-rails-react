9545f875fd8024a9e5d57e6e7370b684b5c345bd
Pablo Bello <thefactus@gmail.com>
2018-04-11 18:21:04 -0300

    update manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,10 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
-//= link_directory ../admin/posts .js
-//= link_directory ../admin/posts_show .js
-//= link_directory ../admin/posts_new .js
-//= link_directory ../home .js
 //= link_directory ../stylesheets .css
+//= link users/sessions.js
+//= link users/sessions.css
+//= link application.css
+// link ../admin/posts.js
+// link ../admin/posts_show.js
+// link ../admin/posts_new.js
+// link ../home.js

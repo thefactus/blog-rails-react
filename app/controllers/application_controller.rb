9d517acd63973f6fed6cdd7887dba4252eab96aa
Pablo Bello <thefactus@gmail.com>
2018-04-16 11:56:48 -0300

    refactor application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   def render_404
     respond_to do |format|
       format.html do
-        render file: Rails.root.join('public', '404'), layout: false, status: '404'
+        render file: Rails.root.join('public', '404'),
+               layout: false, status: '404'
       end
       format.any { head :not_found }
     end

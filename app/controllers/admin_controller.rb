e210ad6eb8e7c41e41da0d322a0b4865984c7695
Pablo Bello <thefactus@gmail.com>
2018-04-16 11:56:35 -0300

    override devise authenticate user method

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,4 +5,14 @@ class AdminController < ApplicationController
   def authenticate_admin!
     render_404 unless current_user.admin?
   end
+
+  private
+
+  def authenticate_user!
+    if admin_user_signed_in?
+      super
+    else
+      redirect_to admin_user_session_url, alert: 'You must sign in'
+    end
+  end
 end

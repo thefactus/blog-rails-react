358b482701416dc9486d52881d7b0263c31d48c3
Pablo Bello <thefactus@gmail.com>
2018-04-16 15:48:41 -0300

    update admin controller auth method

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,15 +1,10 @@
 class AdminController < ApplicationController
-  before_action :authenticate_user!
-  before_action :authenticate_admin!
-
-  def authenticate_admin!
-    render_404 unless current_user.admin?
-  end
+  before_action :authenticate_admin_user!
 
   private
 
-  def authenticate_user!
-    if admin_user_signed_in?
+  def authenticate_admin_user!
+    if admin_user_signed_in? && current_admin_user.admin?
       super
     else
       redirect_to admin_user_session_url, alert: 'You must sign in'

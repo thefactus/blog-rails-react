067c8d5e9b8d460a6e15f03705114570d98d1063
Pablo Bello <thefactus@gmail.com>
2018-04-16 11:55:15 -0300

    put devise routes inside admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
 
   namespace :admin do
     get '', to: 'dashboard#index', as: '/'
     resources :posts
+
+    devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
   end
 end
